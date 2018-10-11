2dc3d3014353b91d435a59de31659a992a650eb6
Adrian Epuras <adrian.epuras@gmail.com>
2018-10-11 09:36:16 +0200

    words - switch languages

--- client/src/containers/Words.js
+++ client/src/containers/Words.js
@@ -2,12 +2,14 @@ import React from "react";
 import axios from "axios-es6";
 import Game from "../components/Game";
 import AddWord from "../components/AddWord";
+import Toggle from "../components/Toggle";
 
 class Words extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            tests: []
+            tests: [],
+            toEnglish: true
         };
     }
 
@@ -30,9 +32,25 @@ class Words extends React.Component {
         axios.post("/openapi/words", word);
     };
 
+    toggleToFrom = () => {
+        this.setState({
+            tests: this.state.tests.map(item => {
+                return {
+                    questions: item.answers,
+                    answers: item.questions
+                };
+            })
+        });
+    };
+
     render() {
         return (
             <div>
+                <Toggle
+                    items={["to english", "to swedish"]}
+                    callback={this.toggleToFrom}
+                    isOn={this.state.toEnglish}
+                />
                 <Game tests={this.state.tests} />
                 <AddWord callback={this.addWord} />
             </div>
