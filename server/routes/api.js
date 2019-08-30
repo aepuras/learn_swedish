c1ef30e386f26e5a7176e849e4dcaccdcf71d6b8
Adrian Epuras <adrian.epuras@gmail.com>
2019-08-30 13:32:56 +0200

    dont know

--- server/routes/api.js
+++ server/routes/api.js
@@ -45,6 +45,123 @@ router.post("/words", (req, res) => {
                     swedish: req.body.newWord.swedish.filter(n => n),
                 };
                 user.words.push(word);
+            } else {
+                let oldWord = req.body.oldWord;
+                oldWord.english = oldWord.english.filter(n => n);
+                oldWord.swedish = oldWord.swedish.filter(n => n);
+                oldWord.helper === "" && delete oldWord.helper;
+                !oldWord.learned && delete oldWord.learned;
+
+                const index = user.words.findIndex(word => {
+                    return (
+                        JSON.stringify(word.english) ===
+                            JSON.stringify(oldWord.english) &&
+                        JSON.stringify(word.swedish) ===
+                            JSON.stringify(oldWord.swedish) &&
+                        (!oldWord.learned ||
+                            word.learned === oldWord.learned) &&
+                        (!oldWord.helper || word.helper === oldWord.helper)
+                    );
+                });
+
+                if (index >= 0) {
+                    user.words[index] = {
+                        english: req.body.newWord.english.filter(n => n),
+                        swedish: req.body.newWord.swedish.filter(n => n),
+                        learned: req.body.newWord.learned,
+                        helper: req.body.newWord.helper,
+                    };
+                }
+            }
+            user.save(err => {
+                if (err) {
+                    console.log(err);
+                    res.status(401).json({
+                        error: "Error saving word",
+                    });
+                } else {
+                    res.send("new word saved");
+                }
+            });
+        }
+    });
+});
+
+router.post("/savesetting", (req, res) => {
+    const userId = getUserId(req);
+    getUser(userId)
+        .then(user => updateUser(user))
+        .catch(err => console.log(err))
+        .then(user => saveUser(user))
+        .catch(err => console.log(err));
+
+
+
+    
+    saveUserData(userId, res, user => {
+        const settingName = req.body.settingName;
+        const settingValue = req.body.settingValue;
+        if(settingName && settingValue) {
+            user.settings[settingName] = settingValue;
+            return true;
+        }
+        return false;
+    });
+});
+
+
+function getUser(userId) {
+    const promise = new Promise((resolve, reject) => {
+        User.findById(userId, (userErr, user) => {
+            if (userErr || !user) {
+                reject(Error(userErr));
+            } else {
+                resolve(user);
+            } 
+        });
+    });
+    return promise;
+}
+
+
+function saveUserData(userId, res, updateUserCallback) {
+    User.findById(userId, (userErr, user) => {
+        if (userErr || !user) {
+            console.log(userErr);
+            res.status(401).json({
+                error: "No user",
+            });
+        } else {
+            if (updateUserCallback(user)) {
+                user.save(err => {
+                    if (err) {
+                        console.log(err);
+                        res.status(401).json({
+                            error: "Error saving word",
+                        });
+                    } else {
+                        res.send("setting saved");
+                    }
+                });
+            }
+        }
+    });
+}
+
+router.post("/words", (req, res) => {
+    User.findById(getUserId(req), (userErr, user) => {
+        if (userErr || !user) {
+            console.log(userErr);
+            res.status(401).json({
+                error: "No user",
+            });
+        } else {
+            if (Object.keys(req.body.oldWord).length === 0) {
+                const word = {
+                    english: req.body.newWord.english.filter(n => n),
+                    swedish: req.body.newWord.swedish.filter(n => n),
+                };
+                user.words.push(word);
 
             } else {
                 let oldWord = req.body.oldWord;
